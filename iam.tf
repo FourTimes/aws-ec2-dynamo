df36b27cbd9744ec1ccfd53daaa34111a6f2cd0c
pavimalar23 <pavimalar23@gmail.com>
2021-07-11 11:54:54 +0530

    ec2 connect with dynamo added

---
+++ iam.tf
@@ -0,0 +1,58 @@
+resource "aws_iam_role" "machine" {
+  name               = "${local.system_name}-machine-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "${local.system_name}-machine-role-policy"
+  path        = "/"
+  description = "dynamodb access iam policy"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:*",
+        "cloudwatch:*",
+        "events:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.machine.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+resource "aws_iam_instance_profile" "machine" {
+  name_prefix = "${local.system_name}-instance"
+  role = aws_iam_role.machine.name
+}
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "public_key" {
+  description = "Public key of the developer to access the EC2 machine"
+  type = string
+	default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDb9KIAkRxVGiYKgwB6RAWxpRAg1U3rNj53mwnTyA42PGirh5JHciF/AER6dlrtIVEwMCZFZS/6O+5BqbjM09f7JZC8sODX2JLiWr7lb1pIm1S4XFIt1ZyoTxsQBOge8CHLPvFY/zF4iH3vIvvLKqI86vDHmZgO4MRbuw67QBLU16WPAZvXKRkyN6ZsMeY6tC3bCg+LwKiN0lw9p3afYVojj37vNbSmifBJJZTMgAkX+oe8FlIwkUt2R76XGhcNd6hcV/yyjbBYTI87RSck9zpUhL8JRakwWewf4iMSRmebSz8W2rteLF+MCJIsyhurNV8iUJy2NIt8kmJn/tdorLj3 sepoy@XPS-13-9360"
+}
